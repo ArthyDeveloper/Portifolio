bf101741035d40feb5957ec30b6c08cd21c88285
Arthy Developer <84215368+ArthyDeveloper@users.noreply.github.com>
2021-05-15 16:47:56 -0300

    Upload v1.1 Portifólio
    Versão para Tablet feita sem consideração a altura da tela.

--- detalhes-slider.js
+++ detalhes-slider.js
@@ -9,18 +9,18 @@ const sliderDetalhes = document.querySelector('.SCP-Slider')
 
 servicosBTN.addEventListener('click', function() {
     sliderServicos.style.left = "50%"
-    sliderCompetencias.style.left = "130%"
-    sliderProjetos.style.left = "160%"
+    sliderCompetencias.style.left = "160%"
+    sliderProjetos.style.left = "190%"
 })
 
 competenciasBTN.addEventListener('click', function() {
-    sliderServicos.style.left = "-60%"
+    sliderServicos.style.left = "-90%"
     sliderCompetencias.style.left = "50%"
-    sliderProjetos.style.left = "160%"
+    sliderProjetos.style.left = "190%"
 })
 
 projetosBTN.addEventListener('click', function() {
-    sliderServicos.style.left = "-60%"
-    sliderCompetencias.style.left = "-30%"
+    sliderServicos.style.left = "-90%"
+    sliderCompetencias.style.left = "-60%"
     sliderProjetos.style.left = "50%"
 })
